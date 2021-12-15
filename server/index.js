984cb0388e3d604235f3b9813c810c7ef876b53a
Kavita Poduri <kavi.pdri@gmail.com>
2021-12-15 12:33:13 -0800

    working on post requests

--- server/index.js
+++ server/index.js
@@ -5,9 +5,22 @@ const PORT = 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello from the server!');
+})
+
+app.get('/api/cows', (req, res) => {
+  res.send('api request to cows');
+})
+
+app.post('/api/cows', (req, res) => {
+  //modify memory
+  console.log(req.body.payload);
+  let payload = JSON.parse(req.body.payload);
+  console.log(typeof req.body.payload);
+  res.send(req.body.payload);
 })
 
 app.listen(PORT, () => {
