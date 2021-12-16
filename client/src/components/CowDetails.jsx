5746192cbef50be5a099ced745de72ce9c47abaf
Kavita Poduri <kavi.pdri@gmail.com>
2021-12-15 16:40:16 -0800

    exported cowdetails lol

---
+++ client/src/components/CowDetails.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class CowDetails extends React.Component {
+  //boilerplate
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>
+        cow details are displayed here
+      </div>
+    )
+  }
+}
+
+export default CowDetails;
